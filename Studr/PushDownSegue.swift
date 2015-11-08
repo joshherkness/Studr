f46dc23427b44eef97389f00224e2f3d5172d369
Joseph Herkness <jpherkness@gmail.com>
2015-11-08 00:13:29 -0500

    Added new segues

---
+++ Studr/PushDownSegue.swift
@@ -0,0 +1,41 @@
+//
+//  PushDownSegue.swift
+//  Studr
+//
+//  Created by Joseph Herkness on 11/7/15.
+//  Copyright © 2015 JJR. All rights reserved.
+//
+
+import UIKit
+
+class PushDownSegue: UIStoryboardSegue {
+
+    override func perform() {
+        // Assign the source and destination views to local variables.
+        let firstVCView = self.sourceViewController.view as UIView!
+        let secondVCView = self.destinationViewController.view as UIView!
+        
+        // Get the screen width and height.
+        let screenWidth = UIScreen.mainScreen().bounds.size.width
+        let screenHeight = UIScreen.mainScreen().bounds.size.height
+        
+        // Specify the initial position of the destination view.
+        secondVCView.frame = CGRectMake(0.0, -screenHeight, screenWidth, screenHeight)
+        
+        // Access the app's key window and insert the destination view above the current (source) one.
+        let window = UIApplication.sharedApplication().keyWindow
+        window?.insertSubview(secondVCView, aboveSubview: firstVCView)
+        
+        // Animate the transition.
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            firstVCView.frame = CGRectOffset(firstVCView.frame, 0.0, screenHeight)
+            secondVCView.frame = CGRectOffset(secondVCView.frame, 0.0, screenHeight)
+            
+            }) { (Finished) -> Void in
+                self.sourceViewController.presentViewController(self.destinationViewController as UIViewController,
+                    animated: false,
+                    completion: nil)
+        }
+        
+    }
+}
